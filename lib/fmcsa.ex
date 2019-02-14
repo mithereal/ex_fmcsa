f04e114a9f39ba59725e4afb4f719ae919299f48
Jason Clark <mithereal@gmail.com>
2019-02-13 22:55:39 -0700

    .

--- lib/fmcsa.ex
+++ lib/fmcsa.ex
@@ -3,33 +3,48 @@ defmodule Fmcsa do
   Documentation for Fmcsa.
   """
 
+ @primary_url "https://ai.fmcsa.dot.gov/hhg/"
+
  @search_url "https://ai.fmcsa.dot.gov/hhg/SearchResults.asp?lan=EN&search=5&ads=a&state="
- 
+
  @states  ["AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY"]
- 
+
  def fetch_companies_by_state(state) do
  response = HTTPotion.get(@search_url <> state)
 
  html = response.body
 
- [result] = Floki.find(html, "table")
+ result = Floki.find(html, "table")
 
- {_, _, table} = result
+ c = Floki.find(result, "td.MiddleTDFMCSA")
 
- [result] = Floki.find(table, "tr.MiddleTDFMCSA")
+ companies = Enum.map(c, fn x ->
+ name = case(x)do
+   {_,_,[{_,[{_,url}],[name]},_]} -> {name, url}
+   {_, _, [city_state]} -> nil
 
- {_, _, tr} = result
- 
- result = Floki.find(tr, "tr:nth-child(1)")
- 
- 
- 
  end
- 
+                      name
+                       end)
+
+Enum.reject(companies, &is_nil/1)
+
+ end
+
  def fetch_company_names do
  names = Enum.map(@states, fn(state) ->
  fetch_companies_by_state(state)
  end)
  end
- 
+
+ def fetch_company_profile(url) do
+    response = HTTPotion.get(@primary_url <> url)
+    html = response.body
+    result = Floki.find(html, "tr.MiddleAltTDFMCSA")
+
+                                                  [  {_, _,    [      _,      _,      {_, _,[name]}    ]},  {_, _,    [     _,      _,      {_, _, [mc]}    ]},  {_, _,    [      _,     _,      {_, _, [ma]}    ]},  _,    [     _,      _,      {_, _, [fax]}    ]},  {_, _,    [      _,      _,      {_, _,        [{_, _, [rating]}]}    ]},  {_, _, _},  {_},  {_, _,    [      _      _,      {_, _, [numberoftrucks]}    ]},  {_, _,    [      _,      _,      {_, _, [numberoftrucks]}    ]}]
+                                                  = result
+    IO.inspect(result)
+ end
+
 end
--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Fmcsa.MixProject do
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-       build_embedded: Mix.env == :prod,
+      build_embedded: Mix.env() == :prod,
       description: description(),
       package: package(),
       elixir: "~> 1.0",
@@ -35,15 +35,15 @@ defmodule Fmcsa.MixProject do
       {:ex_doc, "~> 0.14", only: :dev}
     ]
   end
-  
-    defp description() do
+
+  defp description() do
     "Web scraper for fmcsa.dot.gov."
   end
 
   defp package() do
     [
       name: "fmcsa",
-      files: ["lib",  "mix.exs", "README*"],
+      files: ["lib", "mix.exs", "README*"],
       maintainers: ["Jason Clark"],
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/mithereal/elixir-fcmsa"}
