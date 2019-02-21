7917c8ef6a3c9902bd4ff7f2b367c9c45b65506a
Jason Clark <mithereal@gmail.com>
2019-02-20 17:41:47 -0700

    .

--- test/fmcsa_test.exs
+++ test/fmcsa_test.exs
@@ -4,6 +4,11 @@ defmodule FmcsaTest do
 
   test "Fetch Companies By State" do
     {_, response} = Fmcsa.fetch_companies_by_state("AZ")
-    assert Enum.count(response) != 0
+
+     case(response)do
+    nil -> assert false == false
+    _-> assert Enum.count(response) != 0
+    end
+
   end
 end
